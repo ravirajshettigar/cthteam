ee77712387c6b00ec93053991464f378a5ed2ca5
Raviraj Shettigar (Home) <raviraj.shettigar@gmail.com>
2017-12-17 21:01:46 +0530

    initial code commit

---
+++ src/main/java/internal/cth/CTHWebBootstrap.java
@@ -0,0 +1,10 @@
+package internal.cth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class CTHWebBootstrap {
+    public static void main(String args[]) {
+        SpringApplication.run(CTHWebInitializer.class, args);
+    }
+}
---
+++ src/main/java/internal/cth/CTHWebInitializer.java
@@ -0,0 +1,10 @@
+package internal.cth;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@SpringBootApplication
+public class CTHWebInitializer extends WebMvcConfigurerAdapter {
+}
